249dbaecfe2ec7079f14e53c36c06cd631385eda
Luther Karper <luther.karper@live.com>
2020-06-22 17:09:49 -0400

    Add seed file

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,30 @@
+INSERT INTO bookmarks (id, title, url, description, rating) values
+  (
+    1,
+    'Bookmark!',
+    'http://google.com',
+    'Aliquip incididunt do nulla laboris et nulla qui et nulla et nostrud commodo.',
+    1
+  ),
+  ( 2, 'Pups!', 'http://pups.com', 'Amet enim ex duis eu.', 5 ),
+  (
+    3,
+    'Moar pups!',
+    'http://moarpyps.com',
+    'Elit esse laboris esse ea Lorem.',
+    4
+  ),
+  (
+    4,
+    'All the pups!',
+    'http://puuuuuuups.com',
+    'Nisi cupidatat excepteur elit ullamco.',
+    3
+  ),
+  (
+    5,
+    'doggos!',
+    'http://doggos.com',
+    'Deserunt quis minim velit proident voluptate aliqua.',
+    5
+  );
